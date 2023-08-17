6a17e8e9f2f8888b05988bbedde9347e1bf2d3f9
sh4aadman <sh4adman@gmail.com>
2023-08-17 14:49:20 +0600

    added reset

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -1,3 +1,15 @@
 document.getElementById('log-in').addEventListener('click', function () {
-    
+    const buttonSection = document.getElementById('button-section');
+    buttonSection.classList.add('hidden');
+
+    const formSection = document.getElementById('login-form');
+    formSection.classList.remove('hidden');
+})
+
+document.getElementById('close-confirm').addEventListener('click', function () {
+    const formSection = document.getElementById('login-form');
+    formSection.classList.add('hidden');
+
+    const buttonSection = document.getElementById('button-section');
+    buttonSection.classList.remove('hidden');
 })
