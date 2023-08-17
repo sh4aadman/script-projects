6f609e40ce71d8956f5b51b45a0b9b2ebad97419
sh4aadman <sh4adman@gmail.com>
2023-08-17 15:36:14 +0600

    added validation and username setup

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -13,3 +13,27 @@ document.getElementById('close-confirm').addEventListener('click', function () {
     const buttonSection = document.getElementById('button-section');
     buttonSection.classList.remove('hidden');
 })
+
+document.getElementById('log-in-confirm').addEventListener('click', function () {
+    const emailInputString = document.getElementById('log-in-email');
+    const emailInput = emailInputString.value;
+
+    const passwordInputString = document.getElementById('log-in-password');
+    const passwordInput = passwordInputString.value;
+
+    if (emailInput === 'user@example.com' && passwordInput === 'password') {
+        // user set up
+        window.location.href = 'home.html';
+    }
+    else {
+        window.location.href = 'login.html';
+    }
+})
+
+document.getElementById('log-in-email').addEventListener('keyup', function (event) {
+    const user = event.target.value;
+    const userName = user.slice(0, -10);
+    
+    const setUser = document.getElementById('user');
+    setUser.innerText = userName;
+})
